549a5f937b9306db8b7352a4f754a11928466dde
hassamali2174 <animeboi2174@gmail.com>
2023-04-09 21:19:36 +0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer className='bg-black py-8 text-center text-white'>
+    <div className="container mx-auto">
+      Copyrights &copy; 2023. All rigths reserved
+    </div>
+  </footer>;
+};
+
+export default Footer;
